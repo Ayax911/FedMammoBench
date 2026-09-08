930c6d8ebdc413967083efe5099eec186263c49f
Ayax911 <j.mast8924@gmail.com>
2026-09-08 13:25:23 -0500

    <exp>: Experimento Camilo

--- run.sh
+++ run.sh
@@ -2,12 +2,8 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 configs=(
-  configs/exp28_antioverfit_base.yaml
-  configs/exp29_antioverfit_wd1e2.yaml
-  configs/exp30_antioverfit_labelsmooth.yaml
-  configs/exp31_antioverfit_no_inputdrop.yaml
-  configs/exp32_antioverfit_radimagenet.yaml
-  configs/exp33_pretrain_ablation_imagenet_mismatched_norm.yaml
+  configs/expcamilo.yaml
+
 )
 
 for cfg in "${configs[@]}"; do
